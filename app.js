3454354e2b5bf2b8333e2a90fb54f88c2bfb3fa5
𝐌𝐎𝐇𝐀𝐍𝐍𝐀𝐃 𝐉𝐀𝐖𝐀𝐇 <mohndjawah1@gmail.com>
2026-02-06 16:26:44 +0300

    Update app.js

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ const firebaseConfig = {
   projectId: "kmb-project-e8baa",
   storageBucket: "kmb-project-e8baa.firebasestorage.app",
   messagingSenderId: "76861686082",
-  appId: "1:76861686082:web:acd987feef1dec9a6448db",
-  measurementId: "G-ZVQNTY9GEK"
+  appId: "1:76861686082:web:850319fa7fa82de56448db",
+  measurementId: "G-56SWVHXXQY"
 };
 
 const app = initializeApp(firebaseConfig);
